d4f9ac1370caf6fdb20edc8d46f1117df669bab0
Vicky Vasilopoulou <vickyvasilopoulou@vickys-macbook.lan>
2020-07-08 18:47:09 +0100

    Add the next steps of the form

--- src/FormUserDetails/FormUserDetails.js
+++ src/FormUserDetails/FormUserDetails.js
@@ -1,15 +1,44 @@
-import React from "react";
+import React, { Component } from "react";
+import "./FormUserDetails.css";
+import LoginForm from "../LoginForm/LoginForm";
 
 
-export default function FormUserDetails() {
-    return (
-        <>
-            <label>Choose your level</label>
-            <select className="levels" id="levels">
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-            </select>
-        </>
-    )
+export default class FormUserDetails extends Component {
+    constructor() {
+        super();
+        this.state = {
+            step: 1
+        };
+    }
+
+    nextStep = () => {
+        const { step } = this.state;
+        this.setState({ step: step + 1 });
+    }
+    //Go Back to previous step
+
+    prevStep = () => {
+        const { step } = this.state;
+        this.setState({ step: step - 1 });
+    }
+    render() {
+        const { step } = this.state;
+        switch (step) {
+            case 1:
+                return (
+                    <div className="formUserDetails-container">
+                        <label>Choose your level</label>
+                        <select className="levels" id="levels">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                        </select>
+                        <button className="loginForm-button" onClick={this.nextStep}>Next</button>
+                    </div>
+                )
+            case 2:
+                return <div>Hello</div>
+
+        }
+    }
 }
