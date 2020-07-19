1a8b2930c044a2f3218197c1708db3559a2ee6fc
Vicky Vasilopoulou <vickyvasilopoulou@Vickys-MacBook.local>
2020-07-19 15:20:50 +0100

    Adding header package

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
 import LoginForm from "./LoginForm/LoginForm";
+import Typical from "react-typical";
+
+const steps = [
+  "DramaFit Studio", 5000
+]
 
 function App() {
   return (
     <div className="App">
-      <header>DramaFit Studio</header>
+        <Typical wrapper="span" steps={steps} loop={1} className="header" />
       <LoginForm />
     </div>
   );
